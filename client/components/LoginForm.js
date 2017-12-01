a71b230df36315cf22391d765221cbb4663c6bad
Juri <juri.l@me.com>
2017-12-01 13:35:18 +0200

    Create LoginForm.js

---
+++ client/components/LoginForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  constructor() {
+    super(props);
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        LoginForm
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
